6d249e5ee340aeb2114b3cf6a4bab4b090b050ce
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2022-03-04 14:36:26 -0300

    C04 - Criado componente footer, sem estilizaçao

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,9 +1,19 @@
 import { Component } from 'react';
-
+import twitter from './images/twitter_icon.png';
+import instagram from './images/instagram_icon.png';
+import facebook from './images/facebook_icon.png';
+import github from './images/gitHub_icon.png';
 
 class Footer extends Component {
   render() {
-    return;
+    return (
+      <div className='footer'>
+        <img src={twitter} alt="Twitter Icone" />
+        <img src={facebook} alt="Facebook Icone" />
+        <img src={instagram} alt="Instagram Icone" />
+        <img src={github} alt="Github Icone" />
+      </div>
+    );
   }
 }
 
